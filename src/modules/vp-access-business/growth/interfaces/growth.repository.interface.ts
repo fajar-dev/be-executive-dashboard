94fa6b09e561e4111b692120cbdedd559191ad7f
fajar-dev <fajarrivaldi2015@gmail.com>
2026-06-04 09:28:41 +0700

    fix(growth): add getChurnMrc to IGrowthRepository

--- src/modules/vp-access-business/growth/interfaces/growth.repository.interface.ts
+++ src/modules/vp-access-business/growth/interfaces/growth.repository.interface.ts
@@ -1,5 +1,6 @@
 export interface IGrowthRepository {
     getNewMrc(branchId: string, startDate: string, endDate: string): Promise<{ mrc: number; mrc_unpaid: number; mrc_paid: number }>
+    getChurnMrc(branchId: string, startDate: string, endDate: string): Promise<number>
     getRevenue(branchId: string, startDate: string, endDate: string): Promise<number>
     getLeads(startDate: string, endDate: string): Promise<number>
     getOpportunity(startDate: string, endDate: string): Promise<number>
