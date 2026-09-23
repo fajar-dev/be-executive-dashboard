5c97967428428ccab4cc8a355d6c9e0bf8bdd830
fajar-dev <fajarrivaldi2015@gmail.com>
2026-09-23 08:41:29 +0700

    fix: is not bod

--- src/modules/public/sales-performance/repositories/sales-performance.repository.ts
+++ src/modules/public/sales-performance/repositories/sales-performance.repository.ts
@@ -385,6 +385,7 @@ export class SalesPerformanceRepository implements ISalesPerformanceRepository {
                     AND c.BranchId = '020'
                     AND s.ServiceCategory = 'access_business'
                     AND nci.AccCode LIKE '400%'
+                    AND LOWER(s.ServiceName) NOT LIKE '%service on demand%'
             ),
             discount_data AS (
                 SELECT nci.AI AS ai, ncid.Debet AS discount
